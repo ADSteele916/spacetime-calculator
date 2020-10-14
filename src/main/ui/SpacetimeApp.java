8a5d3e2f65add5089d6f5b9ebff124f3537798a4
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2020-10-14 14:14:33 -0700

    Add methods to SpacetimeApp to list frames and events

--- src/main/ui/SpacetimeApp.java
+++ src/main/ui/SpacetimeApp.java
@@ -1,9 +1,6 @@
 package ui;
 
-import model.Event;
-import model.ReferenceFrame;
-import model.RelativeFrame;
-import model.World;
+import model.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +8,8 @@ import java.util.Scanner;
 
 import static model.World.ALLOWED_DELTA;
 
+
+// Structure of runApp, showCommands, and execute methods from TellerApp example
 public class SpacetimeApp {
     private World world;
     private Scanner input;
@@ -45,8 +44,10 @@ public class SpacetimeApp {
     // EFFECTS: prints the commands available to the user to console
     private void showCommands() {
         System.out.println("\n Commands:");
+        System.out.println("\t lf: List reference frames");
         System.out.println("\t af: Add a reference frame");
         System.out.println("\t rf: Remove a reference frame");
+        System.out.println("\t le: List events");
         System.out.println("\t ae: Add an event");
         System.out.println("\t re: Remove an event");
         System.out.println("\t ve: View all events from a certain frame");
@@ -54,8 +55,14 @@ public class SpacetimeApp {
         System.out.println("\t exit: Exit the program");
     }
 
+    // MODIFIES: this
+    // EFFECTS: runs the appropriate helper method based on which command is inputted
     private void execute(String command) {
-        if (command.equals("af")) {
+        if (command.equals("lf")) {
+            listFrames();
+        } else if (command.equals("le")) {
+            listEvents();
+        } else if (command.equals("af")) {
             addFrame();
         } else if (command.equals("ae")) {
             addEvent();
@@ -76,6 +83,30 @@ public class SpacetimeApp {
         }
     }
 
+    // EFFECTS: lists out all available reference frames
+    private void listFrames() {
+        System.out.println("\nFrames:");
+        MasterFrame master = world.getMasterFrame();
+        System.out.println(master.getName() + " moving at " + master.getVelocity() + "c.");
+        for (ReferenceFrame frame: world.getFrames()) {
+            String speed = " moving at " + frame.getVelocity() + "c relative to " + master.getName() + ".";
+            System.out.println(frame.getName() + speed);
+        }
+    }
+
+    // EFFECTS: lists out all events
+    private void listEvents() {
+        System.out.println("\nEvents:");
+        for (Event event: world.getEvents()) {
+            String name = event.getName();
+            String coords = " with x = " + event.getX() + " light seconds and t = " + event.getTime() + " seconds ";
+            String frame = "in " + event.getFrame().getName() + ".";
+            System.out.println(name + coords + frame);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates a new frame with user-defined name relative velocity
     private void addFrame() {
         String name = selectString("What is the name of this frame?");
         ReferenceFrame frame = selectFrame("What frame are you defining this frame relative to?");
@@ -84,6 +115,8 @@ public class SpacetimeApp {
         System.out.println("Frame added!");
     }
 
+    // MODIFIES: this
+    // EFFECTS: creates a new event with user-defined time, x, name, and frame
     private void addEvent() {
         String name = selectString("What is the name of this event?");
         ReferenceFrame frame = selectFrame("Which frame would you like to define the event in?");
@@ -93,16 +126,29 @@ public class SpacetimeApp {
         System.out.println("Event added!");
     }
 
+    // MODIFIES: this
+    // EFFECTS: removes a given relative frame and moves all events defined in it to the masterFrame
     private void removeFrame() {
         RelativeFrame frame = selectRelativeFrame("Which frame would you like to delete?");
+        for (Event event: world.getEvents()) {
+            if (event.getFrame() == frame) {
+                event.changeFrame(world.getMasterFrame());
+            }
+        }
         world.getMasterFrame().removeRelativeFrame(frame);
+        String masterName = world.getMasterFrame().getName();
+        System.out.println("Removed " + frame.getName() + "and moved all events defined in it to " + masterName + "!");
     }
 
+    // MODIFIES: this
+    // EFFECTS: removes a given event
     private void removeEvent() {
         Event event = selectEvent("Which event would you like to delete?");
         world.removeEvent(event);
+        System.out.println("Removed " + event.getName() + "!");
     }
 
+    // EFFECTS: shows the position and time of all events from the perspective of a a certain frame
     private void viewEvents() {
         ReferenceFrame frame = selectFrame("What frame would you like to view the events from?");
         List<Event> transformedEvents = new ArrayList<>();
@@ -114,6 +160,8 @@ public class SpacetimeApp {
         }
     }
 
+    // EFFECTS: calculates the Lorentz Invariant of two events, then says if they are
+    //          spacelike, timelike, or null separated
     private void lorentzInvariant() {
         if (world.getEvents().isEmpty()) {
             System.out.println("You don't have any events to get the Lorentz Invariant of!");
@@ -135,6 +183,7 @@ public class SpacetimeApp {
         }
     }
 
+    // EFFECTS: prompts the user to select any ReferenceFrame in the world
     private ReferenceFrame selectFrame(String prompt) {
         System.out.println("\n" + prompt);
         System.out.println("(0) " + world.getMasterFrame().getName());
@@ -157,6 +206,7 @@ public class SpacetimeApp {
         }
     }
 
+    // EFFECTS: prompts the user to select any RelativeFrame in the world
     private RelativeFrame selectRelativeFrame(String prompt) {
         System.out.println("\n" + prompt);
         for (int i = 0; i < world.getFrames().size(); i++) {
@@ -176,6 +226,7 @@ public class SpacetimeApp {
         }
     }
 
+    // EFFECTS: prompts the user to select any Event in the world
     private Event selectEvent(String prompt) {
         System.out.println("\n" + prompt);
         for (int i = 0; i < world.getEvents().size(); i++) {
@@ -195,16 +246,19 @@ public class SpacetimeApp {
         }
     }
 
+    // EFFECTS: prompts the user to input a string
     private String selectString(String prompt) {
         System.out.println("\n" + prompt);
         return input.next();
     }
 
+    // EFFECTS: prompts the user to input a double
     private double selectDouble(String prompt) {
         System.out.println("\n" + prompt);
         return input.nextDouble();
     }
 
+    // EFFECTS: prompts the user to input a double within the given bounds
     private double selectDouble(String prompt, double min, double max) {
         System.out.println("\n" + prompt);
         double value = input.nextDouble();
